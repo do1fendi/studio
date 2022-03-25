0b9a74451fe5a6f2ae85e681022b4be8ce8cff1d
Siloah <admin@Siloahs-MacBook-Pro.local>
2022-03-25 14:03:43 +0800

    index modify

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,13 +5,28 @@ const Home: NextPage = () => {
   return (
     // <div className="h-screen" style={{ backgroundImage: `url('${process.env.BASEURL}/images/bg/banner-bg.jpg')`}}>
     <div className="">
-      <h1>HOME</h1>
-      <div className="flex justify-center p-5">
+      {/* <h1>HOME</h1> */}
+
+      <div className="relative flex items-center justify-center mb-12 overflow-hidden p-8">
         <Image
           src={`${process.env.BASEURL}/images/logo/logo.png`}
-          width="650"
-          height="400"
+        
+          width={450}
+          height={200}
+          className="z-30"
         />
+        <video
+          autoPlay
+          loop
+          muted
+          className="absolute z-10 w-auto min-w-full min-h-full max-w-none bg-blend-overlay opacity-40"
+        >
+          <source
+            src={`${process.env.BASEURL}/videos/bg/file.mp4`}
+            type="video/mp4"
+          />
+          Your browser does not support the video tag.
+        </video>
       </div>
 
       <div className="w-1/4 h-48 relative">
