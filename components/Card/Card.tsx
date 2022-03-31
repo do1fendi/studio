72327db5bfa2231d798c62f879298c2d8bb3d7d9
Siloah <admin@Siloahs-MacBook-Pro.local>
2022-03-31 18:12:16 +0800

    modify project image structure

--- components/Card/Card.tsx
+++ components/Card/Card.tsx
@@ -10,7 +10,7 @@ type CardType = {
 const Card = (props: CardType) => {
   return (
     <div className="flex flex-col justify-center items-center">
-      <Image src={props.picUrl} width={650} height={400} />
+      <Image src={props.picUrl} width={650} height={400} alt={props.title} />
       <h3 className="text-xl font-bold text-gray-600">{props.title}</h3>
       <h3 className="text-xl font-bold text-gray-400">{props.category}</h3>
     </div>
